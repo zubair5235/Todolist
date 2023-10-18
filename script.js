28577cee66ce80853bf12954eed5773dfc4cfef8
zubair5235 <mohamedzubair235@gmail.com>
2023-10-18 07:56:34 +0530

    uploading via git

---
+++ script.js
@@ -0,0 +1,32 @@
+var AddTaskButton = document.getElementById("addTask-btn");
+var TextEditor = document.getElementById("textEditor");
+var SaveButton = document.getElementById("save-btn");
+var ListContainer = document.getElementById("listContainer");
+
+
+function displayEditor()
+{
+    AddTaskButton.classList.add("clickDesign");
+    TextEditor.style.display = "block";
+}
+
+function displaySaveButton()
+{
+    SaveButton.style.display = "block";
+}
+
+function saveWork()
+{
+    var ListItem = document.createElement("li");
+    ListItem.textContent = TextEditor.value;
+    ListItem.innerHTML = ListItem.textContent + "<button id='delete-btn' onclick='deleteWork(event)'>Delete</button>";
+    ListContainer.append(ListItem);
+    SaveButton.style.display = "none";
+    TextEditor.style.display = "none";
+    TextEditor.value = "";
+}
+
+function deleteWork(event)
+{
+    event.target.parentElement.remove();
+}
